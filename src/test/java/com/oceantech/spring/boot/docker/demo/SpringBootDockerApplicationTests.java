3e977ad3109c4831e5358e7303948ca373d984ab
sagarmohanty <sagar.mohanty@qualibar.com>
2022-07-06 21:25:37 -0700

    code changes

---
+++ src/test/java/com/oceantech/spring/boot/docker/demo/SpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oceantech.spring.boot.docker.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
